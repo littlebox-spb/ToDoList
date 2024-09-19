accb0c65d5fb882eae5cb9a0e5adc01c1c4c3db7
Alexander Altukhov <sasha@littlebox.spb.ru>
2024-09-19 18:05:17 +0300

    modified:   src/todolist/core/admin.py

--- src/todolist/core/admin.py
+++ src/todolist/core/admin.py
@@ -1,3 +1,37 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from core.models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    """Какие поля отображаются на странице списка для изменения из интерфейса администратора"""
+
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+    )
+    """Поиск по полям"""
+    search_fields = (
+        "email",
+        "first_name",
+        "last_name",
+        "username",
+    )
+    """Делает все поля доступными только для чтения"""
+    readonly_fields = ("last_login", "date_joined")
+    """ Автоматически добавит фильтр этого поля на стороне администратора"""
+    list_filter = (
+        "is_staff",
+        "is_active",
+        "is_superuser",
+    )
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        ("Personal Info", {"fields": ("first_name", "last_name", "email")}),
+        ("Permission", {"fields": ("is_active", "is_staff", "is_superuser")}),
+        ("Impotent dates", {"fields": ("last_login", "date_joined")}),
+    )
