43c91a83be88ef34ab64469115d4ce0af73463ef
Alexander Altukhov <sasha@littlebox.spb.ru>
2024-10-13 19:37:39 +0300

    modified:   entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+set -e
+
 # Переход в нужную директорию
 cd /opt/todolist || exit 1
 
 # Проверка миграций
-python manage.py migrate --check
+python manage.py migrate
 status=$?
-
 if [[ $status != 0 ]]; then
   # Выполнение миграций, если есть изменения
   python manage.py migrate
