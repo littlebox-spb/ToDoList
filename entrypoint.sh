6d17a7fffd3bcfe0a2586bc91cc7efafa1674330
Alexander Altukhov <sasha@littlebox.spb.ru>
2024-10-13 14:46:43 +0300

    modified:   Dockerfile 	new file:   Dockerfile.bak 	modified:   docker-compose.yaml 	new file:   docker-compose.yaml.bak 	modified:   entrypoint.sh 	new file:   entrypoint.sh.bak

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+
+# Переход в нужную директорию
+cd /opt/todolist || exit 1
+
+# Проверка миграций
 python manage.py migrate --check
 status=$?
+
 if [[ $status != 0 ]]; then
+  # Выполнение миграций, если есть изменения
   python manage.py migrate
 fi
+
+# Запуск основной команды
 exec "$@"
