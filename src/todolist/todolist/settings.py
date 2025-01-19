32aa1ec31e8c8a5ace4e145e585a7437d557ec64
littlebox-spb <altukhov.alexandr@mail.ru>
2025-01-19 04:23:20 +0300

    modified:   docker-compose.yaml 	modified:   src/todolist/todolist/settings.py

--- src/todolist/todolist/settings.py
+++ src/todolist/todolist/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": "127.0.0.1",
+        "HOST": "localhost",
         "PORT": "5432",
     }
 }
