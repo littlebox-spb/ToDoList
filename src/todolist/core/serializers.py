e68a77e8096c4055578f8f9d3133ae06cf51ef8a
littlebox-spb <altukhov.alexandr@mail.ru>
2025-01-20 20:28:59 +0300

    modified:   src/todolist/core/serializers.py

--- src/todolist/core/serializers.py
+++ src/todolist/core/serializers.py
@@ -1,38 +1,38 @@
-# from django.contrib.auth.hashers import make_password
-# from django.contrib.auth.password_validation import validate_password
-# from rest_framework import serializers
-# from rest_framework.exceptions import ValidationError
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
-# from .models import User
-
-
-# class PasswordField(serializers.CharField):
-
-#     def __init__(self, **kwargs):
-#         kwargs["style"] = {"input_type": "password"}
-#         kwargs.setdefault("write_only", True)
-#         super().__init__(**kwargs)
-#         self.validators.append(validate_password)
+from .models import User
 
 
-# class CreateUserSerializer(serializers.ModelSerializer):
-#     password = PasswordField(required=True)
-#     password_repeat = PasswordField(required=True)
+class PasswordField(serializers.CharField):
 
-#     class Meta:
-#         model = User
-#         fields = ("id", "username", "first_name", "last_name", "email", "password", "password_repeat")
+    def __init__(self, **kwargs):  # type: ignore[no-untyped-def]
+        kwargs["style"] = {"input_type": "password"}
+        kwargs.setdefault("write_only", True)
+        super().__init__(**kwargs)
+        self.validators.append(validate_password)
 
-#     def validate(self, attrs: dict):
-#         """обязательное поле поэтому квадратные скобки,
-#         если не через get != attrs["password_repeat"]:"""
-#         if attrs["password"]:
-#             raise ValidationError(
-#                 "Password must match"
-#             )  # Импортируем из from rest_framework.exceptions import ValidationError
-#         return attrs
 
-#     def create(self, validated_data: dict):
-#         del validated_data["password_repeat"]  # удаление
-#         validated_data["password"] = make_password(validated_data["password"])  # шифровка пароля
-#         return super().create(validated_data)
+class CreateUserSerializer(serializers.ModelSerializer):
+    password = PasswordField(required=True)
+    password_repeat = PasswordField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("id", "username", "first_name", "last_name", "email", "password", "password_repeat")
+
+    def validate(self, attrs: dict):  # type: ignore[no-untyped-def]
+        """обязательное поле поэтому квадратные скобки,
+        если не через get != attrs["password_repeat"]:"""
+        if attrs["password"]:
+            raise ValidationError(
+                "Password must match"
+            )  # Импортируем из from rest_framework.exceptions import ValidationError
+        return attrs
+
+    def create(self, validated_data: dict):  # type: ignore[no-untyped-def]
+        del validated_data["password_repeat"]  # удаление
+        validated_data["password"] = make_password(validated_data["password"])  # шифровка пароля
+        return super().create(validated_data)
